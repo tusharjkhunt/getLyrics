7207cb66fea2d957933c5d652f19b5d485521288
Tushar Khunt <tusharkhunt121@gmail.com>
2019-09-24 04:11:30 -0400

    Updates

---
+++ precache-manifest.ef1f7af7a94bce1ed02b8dc32dbc12b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b853f50fcf304a9d6914b66e7bacf8",
+    "url": "/getLyrics/index.html"
+  },
+  {
+    "revision": "270ccabfbb3119a34fc0",
+    "url": "/getLyrics/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "53a4443990eeb12de0ec",
+    "url": "/getLyrics/static/js/2.e9301cee.chunk.js"
+  },
+  {
+    "revision": "270ccabfbb3119a34fc0",
+    "url": "/getLyrics/static/js/main.1aab074c.chunk.js"
+  },
+  {
+    "revision": "f868608691774b4dced7",
+    "url": "/getLyrics/static/js/runtime-main.7a4b7185.js"
+  }
+]);
